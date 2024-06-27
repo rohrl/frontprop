f57997598fceb2febf1a810bfb7b4a98fd74d6d1
rohrl <karol.pajak@gmail.com>
2024-06-27 20:08:09 +1000

    Analysis on simple patterns: plot active neurons per pattern (histogram over 100 runs)

--- fp_datasets.py
+++ fp_datasets.py
@@ -3,85 +3,100 @@ from typing import List
 import numpy as np
 import torch
 
-ALL_PATTERNS = [t.float() for t in [
-    torch.tensor([
-        [1, 0, 0, 0, 0, 0, 1],
-        [0, 1, 0, 0, 0, 1, 0],
-        [0, 0, 1, 0, 1, 0, 0],
-        [0, 0, 0, 1, 0, 0, 0],
-        [0, 0, 1, 0, 1, 0, 0],
-        [0, 1, 0, 0, 0, 1, 0],
-        [1, 0, 0, 0, 0, 0, 1]
-    ]),
-    torch.tensor([
-        [0, 0, 0, 1, 0, 0, 0],
-        [0, 0, 0, 1, 0, 0, 0],
-        [0, 0, 0, 1, 0, 0, 0],
-        [1, 1, 1, 1, 1, 1, 1],
-        [0, 0, 0, 1, 0, 0, 0],
-        [0, 0, 0, 1, 0, 0, 0],
-        [0, 0, 0, 1, 0, 0, 0]
-    ]),
-    torch.tensor([
-        [0, 0, 1, 1, 1, 0, 0],
-        [0, 1, 0, 0, 0, 1, 0],
-        [1, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 1],
-        [0, 1, 0, 0, 0, 1, 0],
-        [0, 0, 1, 1, 1, 0, 0]
-    ]),
-    torch.tensor([
-        [0, 1, 0, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0, 0]
-    ]),
-    torch.tensor([
-        [1, 1, 1, 0, 0, 0, 0],
-        [1, 1, 1, 0, 0, 0, 0],
-        [1, 1, 1, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0]
-    ]),
-    torch.tensor([
-        [0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 1, 1, 0],
-        [0, 0, 0, 0, 1, 1, 0],
-        [0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0],
-    ])]]
 
+class SimplePatterns:
+    DIMS = [7, 7]
+    ALL_PATTERNS = [t.float() for t in [
+        torch.tensor([
+            [1, 0, 0, 0, 0, 0, 1],
+            [0, 1, 0, 0, 0, 1, 0],
+            [0, 0, 1, 0, 1, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0],
+            [0, 0, 1, 0, 1, 0, 0],
+            [0, 1, 0, 0, 0, 1, 0],
+            [1, 0, 0, 0, 0, 0, 1]
+        ]),
+        torch.tensor([
+            [0, 0, 0, 1, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0],
+            [1, 1, 1, 1, 1, 1, 1],
+            [0, 0, 0, 1, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0]
+        ]),
+        torch.tensor([
+            [0, 0, 1, 1, 1, 0, 0],
+            [0, 1, 0, 0, 0, 1, 0],
+            [1, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 1],
+            [0, 1, 0, 0, 0, 1, 0],
+            [0, 0, 1, 1, 1, 0, 0]
+        ]),
+        torch.tensor([
+            [0, 1, 0, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0, 0, 0]
+        ]),
+        torch.tensor([
+            [1, 1, 1, 0, 0, 0, 0],
+            [1, 1, 1, 0, 0, 0, 0],
+            [1, 1, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0]
+        ]),
+        torch.tensor([
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 1, 0],
+            [0, 0, 0, 0, 1, 1, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+        ])]]
 
-def get_simple_pattern(probs: List[float] = None, noise=0.0):
-    """
-    Randomly select a pattern given probabilities.
-    """
-    if probs is None:
-        probs = [1.0] * len(ALL_PATTERNS)
+    def __init__(self):
+        raise ValueError("This class should not be instantiated.")
 
-    if len(probs) > len(ALL_PATTERNS):
-        raise ValueError(f"Only {len(ALL_PATTERNS)} patterns available.")
+    @staticmethod
+    def get_simple_pattern(probs: List[float] = None, noise=0.0) -> torch.Tensor:
+        """
+        Randomly select a pattern given probabilities.
+        """
+        if probs is None:
+            probs = [1.0] * len(SimplePatterns.ALL_PATTERNS)
 
-    probs /= np.sum(probs)
+        if len(probs) > len(SimplePatterns.ALL_PATTERNS):
+            raise ValueError(f"Only {len(SimplePatterns.ALL_PATTERNS)} patterns available.")
 
-    idx = np.random.choice(len(probs), p=probs)
+        probs /= np.sum(probs)
 
-    # add gaussian noise to the pattern
-    if noise > 0:
-        pattern = ALL_PATTERNS[idx].clone()
-        pattern += torch.randn_like(pattern) * noise
-        return pattern
+        idx = np.random.choice(len(probs), p=probs)
 
-    return ALL_PATTERNS[idx]
+        # add gaussian noise to the pattern
+        if noise > 0:
+            pattern = SimplePatterns.ALL_PATTERNS[idx].clone()
+            pattern += torch.randn_like(pattern) * noise
+            return pattern
 
+        return SimplePatterns.ALL_PATTERNS[idx]
 
-def get_all_patterns(n: int = len(ALL_PATTERNS)):
-    return ALL_PATTERNS[:n]
+    @staticmethod
+    def get_all_patterns(n: int = None) -> List[torch.Tensor]:
+        n = n or len(SimplePatterns.ALL_PATTERNS)
+        return SimplePatterns.ALL_PATTERNS[:n]
+
+    @staticmethod
+    def get_all_patterns_count():
+        return len(SimplePatterns.ALL_PATTERNS)
+
+    @staticmethod
+    def get_pattern_dims():
+        return SimplePatterns.DIMS
